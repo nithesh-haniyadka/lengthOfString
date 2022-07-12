15da829657b34f593f7c698b8b65f413ec010a52
DESKTOP-B65V3QH\Admin <nithesh.k@honeywell.com>
2022-07-12 21:29:36 +0530

    null value validation in test cases

--- unitTestcases/UnitTest1.cs
+++ unitTestcases/UnitTest1.cs
@@ -5,10 +5,10 @@ namespace unitTestcases
 {
 
     
-
-   // Create a unit test project using Xunit and write test cases to find length of string for "", "123", "123456789010", NULL
+// Create a unit test project using Xunit and write test cases to find length of string for "", "123", "123456789010", NULL
     public static class UnitTest1
     {
+        
         [Fact]
         public static void checkUnitTest()
         {
@@ -30,13 +30,23 @@ namespace unitTestcases
         [Fact]
         public static void checkUnitTest2()
         {
-
             var test = LengthHelper.findLength("123456789010");
             Assert.Equal(12, test);
 
         }
 
-      
+        [Fact]
+     
+        public static void NullOrEmptyTest()
+        {
+            string str = null;
+            bool b3 = string.IsNullOrEmpty(str);
+            Assert.True(b3);
+
+
+        }
+
+
 
     }
 }
