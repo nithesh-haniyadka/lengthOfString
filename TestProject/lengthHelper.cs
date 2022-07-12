32634cf3023038f2602ad49661908838bdd14dad
DESKTOP-B65V3QH\Admin <nithesh.k@honeywell.com>
2022-07-12 21:07:58 +0530

    test cases included

--- TestProject/lengthHelper.cs
+++ TestProject/lengthHelper.cs
@@ -5,31 +5,35 @@ using System.IO;
 
 
 
-class LengthHelper
+namespace findLengthOftheString
 {
-	public static int findLength(string str)
-    {
-		int l=0;
-
-		foreach (char chr in str)
-		{
-			l += 1;
-		}
-		return l; 
-
-	}
-
-	// Main Method
-	public static void Main()
+	public class LengthHelper
 	{
-		string str;
-		int l = 0;
-		Console.WriteLine("Enter the string :");
-		str = Console.ReadLine();
-		l = findLength(str);
+		public static int findLength(string str)
+		{
+			int l = 0;
 
-		Console.Write("Length of the string is : {0}\n\n", l);
+			foreach (char chr in str)
+			{
+				l += 1;
+			}
+			return l;
+
+		}
+
+		// Main Method
+		public static void Main()
+		{
+			string str;
+			int l = 0;
+			Console.WriteLine("Enter the string :");
+			str = Console.ReadLine();
+			l = findLength(str);
+
+			Console.Write("Length of the string is : {0}\n\n", l);
 
 
+		}
 	}
+
 }
--- unitTestcases/UnitTest1.cs
+++ unitTestcases/UnitTest1.cs
@@ -1,105 +1,42 @@
+using findLengthOftheString;
+
+
 namespace unitTestcases
 {
 
+    
+
    // Create a unit test project using Xunit and write test cases to find length of string for "", "123", "123456789010", NULL
-    public class UnitTest1
+    public static class UnitTest1
     {
-        string s1 = "";
         [Fact]
-        public void Test1()
+        public static void checkUnitTest()
         {
-            if (s1 == "")
-            {
-                Console.WriteLine("s1 equals empty string.");
-            }
+             
+            var test = LengthHelper.findLength("123");
+            Assert.Equal(3, test);
 
         }
-        [Fact]
-        public void LengthTest()
-    {
-        // Satisfies rule: TestForEmptyStringsUsingStringLength.
-        if (s1 != null && s1.Length == 0)
-        {
-            Console.WriteLine("s1.Length == 0.");
-        }
-    }
-        [Fact]
-        public void NullOrEmptyTest()
-        {
-            // Satisfies rule: TestForEmptyStringsUsingStringLength.
-            if (!String.IsNullOrEmpty(s1))
-            {
-                Console.WriteLine("s1 != null and s1.Length != 0.");
-            }
-        }
 
-        string s2 = "123";
         [Fact]
-        public void Test2()
+        public static void checkUnitTest1()
         {
-            if (s2 == "123")
-            {
-                Console.WriteLine("s1 not equals to empty string");
-            }
+
+            var test = LengthHelper.findLength("");
+            Assert.Equal(0, test);
 
         }
-        [Fact]
-        public void LengthTest2()
-        {
-            // Satisfies rule: TestForEmptyStringsUsingStringLength.
-            if (s2 != null && s1.Length != 0)
-            {
-                Console.WriteLine("length is not equals to 0");
-            }
-        }
-        [Fact]
-        public void NullOrEmptyTest2()
-        {
-            // Satisfies rule: TestForEmptyStringsUsingStringLength.
-            if (!String.IsNullOrEmpty(s2))
-            {
-                Console.WriteLine("not equal to empty or Null");
-            }
-        }
 
-
-        string s3 = "123456789010";
         [Fact]
-        public void Test3()
+        public static void checkUnitTest2()
         {
-            if (s3 == "123456789010")
-            {
-                Console.WriteLine("s3 not equals to empty string");
-            }
+
+            var test = LengthHelper.findLength("123456789010");
+            Assert.Equal(12, test);
 
         }
-        [Fact]
-        public void LengthTest3()
-        {
-            // Satisfies rule: TestForEmptyStringsUsingStringLength.
-            if (s3 != null && s3.Length != 0)
-            {
-                Console.WriteLine("length is not equals to 0");
-            }
-        }
-        [Fact]
-        public void NullOrEmptyTest3()
-        {
-            // Satisfies rule: TestForEmptyStringsUsingStringLength.
-            if (!String.IsNullOrEmpty(s3))
-            {
-                Console.WriteLine("not equal to empty or Null");
-            }
-        }
 
-        string s4 = null;
-            public void NullOrEmptyTest4()
-        {
-            // Satisfies rule: TestForEmptyStringsUsingStringLength.
-            if (!String.IsNullOrEmpty(s4))
-            {
-                Console.WriteLine("s4 is null");
-            }
-        }
+      
+
     }
 }
